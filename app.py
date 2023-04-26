e325c96f911a7a59aa937436c95e720a3c501351
Tanya Shylock <tourtanya@gmail.com>
2023-04-26 13:36:26 -0600

    starter code, requirements

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+# TODO: add classes to from models once created
+from models import db, connect_db
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- models.py
+++ models.py
@@ -1,1 +1,11 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy() #create a SQLAlchemy instance
+#create a function that ties your db object to your app object
+#thus, allows your flask app to connect to the specified db
+def connect_db(app):
+   """Connect to database."""
+   app.app_context().push()
+   db.app = app
+   db.init_app(app)
