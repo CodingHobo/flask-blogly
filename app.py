752d70ca74dfdde31eb9422252cbaf1ad0c7d052
Russell Jones <justyrones@gmail.com>
2023-04-26 18:19:41 -0600

    done routes

--- app.py
+++ app.py
@@ -21,9 +21,37 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def go_home():
-    return redirect('home.html')
-
+    return redirect('/users')
 
 @app.get('/users')
 def show_users():
-    User.query.
+    #retrieve users and pass users to users.html page to be rendered
+
+    return render_template("users.html")
+
+@app.get('/users/new')
+def user_form():
+    return render_template("new_user_form.html")
+
+@app.post('/users/new')
+def add_user():
+    #process the form info adding the new user to DB
+    return redirect("/users")
+
+@app.get('/users/<int:user-id>')
+def show_info():
+    #show info of given user <user-id>
+    return render_template("user_details.html")
+
+@app.get('/users/<int:user-id>/edit')
+def edit_user():
+    return render_template("user_edit.html")
+
+@app.post('/users/<int:user-id>/edit')
+def edit_and_redirect():
+    return redirect("/users")
+
+@app.post('/users/<int:user-id>/delete')
+def delete_and_redirect():
+    return redirect("/users")
+
