078920ae783f7b8a011f11b27d3b35b9d6965743
Russell Jones <justyrones@gmail.com>
2023-04-27 11:08:23 -0600

    adding links to users

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def go_home():
 @app.get('/users')
 def show_users():
     #retrieve users and pass users to users.html page to be rendered
-
-    return render_template("users.html")
+    all_users = User.query.all()
+    return render_template("users.html", all_users=all_users)
 
 @app.get('/users/new')
 def user_btn():
@@ -36,22 +36,30 @@ def user_btn():
 @app.post('/users/new')
 def add_user():
     #process the form info adding the new user to DB
+    info = request.form
+    new_user = User(first_name = info["first_name"],
+                    last_name = info["last_name"],
+                    image_url = info["image_url"])
+
+    db.session.add(new_user)
+    db.session.commit()
     return redirect("/users")
 
-@app.get('/users/<int:user-id>')
-def show_info():
+@app.get('/users/<int:user_id>')
+def show_info(user_id):
     #show info of given user <user-id>
+
     return render_template("user_details.html")
 
-@app.get('/users/<int:user-id>/edit')
-def edit_user():
+@app.get('/users/<int:user_id>/edit')
+def edit_user(user_id):
     return render_template("user_edit.html")
 
-@app.post('/users/<int:user-id>/edit')
-def edit_and_redirect():
+@app.post('/users/<int:user_id>/edit')
+def edit_and_redirect(user_id):
     return redirect("/users")
 
-@app.post('/users/<int:user-id>/delete')
-def delete_and_redirect():
+@app.post('/users/<int:user_id>/delete')
+def delete_and_redirect(user_id):
     return redirect("/users")
 
